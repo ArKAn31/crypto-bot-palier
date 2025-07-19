7a657f5852e56eeb9aa46cd205ae882dcb36bb85
ArKAn31 <tom65.arque@gmail.com>
2025-07-19 18:40:22 +0200

    Delete set_webhook.py

--- set_webhook.py
+++
@@ -1,8 +0,0 @@
-import os
-from telegram import Bot
-
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-WEBHOOK_URL = f"https://crypto-bot-palier.onrender.com/{TOKEN}"
-
-bot = Bot(token=TOKEN)
-print(bot.set_webhook(url=WEBHOOK_URL))
